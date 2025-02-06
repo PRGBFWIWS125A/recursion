09910e18e160a1a77c2ccef3ec288dd9760347f3
NikFl <niklas@flieck.de>
2025-02-06 11:25:16 +0100

    addFirst

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -1,2 +1,10 @@
 public record SinglyLinkedList(ListNode root) {
+
+    static SinglyLinkedList addFirst(final SinglyLinkedList list, final int value){
+        
+        return new SinglyLinkedList(new ListNode(value, list.root));
+
+
+    }
 }
+
