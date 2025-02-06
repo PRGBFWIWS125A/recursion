cbe43a013a3d24e06142e74261d5299be63a6282
Thomas Ströder <thomas.stroeder@email.de>
2025-02-06 11:20:50 +0000

    toString

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -1,10 +1,19 @@
 public record SinglyLinkedList(ListNode root) {
 
     static SinglyLinkedList addFirst(final SinglyLinkedList list, final int value){
-        
-        return new SinglyLinkedList(new ListNode(value, list.root));
-
-
+        return new SinglyLinkedList(new ListNode(value, list.root()));
     }
+    
+    static String toString(final SinglyLinkedList list) {
+        return toString(list.root()); 
+    }
+    
+    static String toString(final ListNode node) {
+        if ( node == null ) 
+            return ""; 
+        if (node.next() == null)  
+            return String.valueOf(node.value());   
+        return String.valueOf(node.value()) + ", " + toString(node.next());
+    }
+    
 }
-
